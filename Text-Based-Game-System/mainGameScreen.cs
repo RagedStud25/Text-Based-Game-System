ed1b7cf1252a647ecd99a03146a53641c41f5b76
Hannah Bianca Abacan <abacan.263127@meycauayan.sti.edu.ph>
2024-01-01 23:30:51 +0800

    - added new methods - fixed ui - fixed case

--- Text-Based-Game-System/mainGameScreen.cs
+++ Text-Based-Game-System/mainGameScreen.cs
@@ -14,11 +14,14 @@ namespace Text_Based_Game_System
 {
     public partial class mainGameScreen : Form
     {
-        int choice = 1; //Get from database. THis will serve as the save point on the user.
+        //Get from database. THis will serve as the save point on the user.
+
+        int choice = 1; 
 
         public bool btnChoiceOneClicked = false;
         public bool btnChoiceTwoClicked = false;
         public bool btnChoiceThreeClicked = false;
+        public bool btnContinueClicked = false;
 
         public mainGameScreen()
         {
@@ -46,6 +49,12 @@ namespace Text_Based_Game_System
             gameStart();
         }
 
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            btnContinueClicked = true;
+            gameStart();
+        }
+
         private void labelHome_Click(object sender, EventArgs e)
         {
             startScreen startScreen = new startScreen();
@@ -54,7 +63,7 @@ namespace Text_Based_Game_System
 
         static int GetLatestPlayerID()
         {
-            string connectionString = "Data Source=LAPTOP-KJTSSLLV\\SQLEXPRESS;Initial Catalog=DB_TextBasedGameSystem;Integrated Security=True";
+            string connectionString = "Data Source=DESKTOP-O47AIOR\\SQLEXPRESS;Initial Catalog=DB_TextBasedGameSystem;Integrated Security=True";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -71,7 +80,7 @@ namespace Text_Based_Game_System
 
         void GetPlayerStats(int playerId) {
 
-            string connectionString = "Data Source=LAPTOP-KJTSSLLV\\SQLEXPRESS;Initial Catalog=DB_TextBasedGameSystem;Integrated Security=True";
+            string connectionString = "Data Source=DESKTOP-O47AIOR\\SQLEXPRESS;Initial Catalog=DB_TextBasedGameSystem;Integrated Security=True";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
@@ -128,13 +137,82 @@ namespace Text_Based_Game_System
 
         }
 
-        public void mainStory_1() //Main story 1
-        {
-            labelMainstory.Text = "MAIN GAME STORY (1) \n Choices: \n A. Maitim \n B. Maputi \n C. Snigger";
+        // main story 1
 
-            if (btnChoiceOneClicked == true) //CHOICE A
+        public void mainStory_1() 
+        {
+            labelMainstory.Text = "On a Monday morning, you woke up to the sound of your alarm. Today's your first day working as a waiter at a restaurant called \"Joblibee.\" You're nervous right now because you remembered that it's a famous restaurant and the interviewer told you that you must be able to work under pressure. \r\n\r\n\"I can do this!\" you said to yourself, while looking at the mirror, placing your nameplate on your polo. \r\n \nClick Continue to Proceed.";
+
+                btnContinue.Enabled = true;
+                btnContinue.Visible = true;
+                choice++;
+        }
+
+        // filler 1
+        // kulang ng probability pa sa choice b kasi dalawa possible outcome
+        // letter b intelligence based
+
+        public void fillerStory_1()
+        {
+            labelMainstory.Text = "While you're on your way to work, you saw an old woman struggling on her way home as the field that she's walking on has a lot of stairs. \r\n\r\na. help her and be late\r\nb. ask someone near you to help the old woman (50% success)\r\nc. pretend that you didn't see her and walk away\r\n\n";
+
+            if (btnChoiceOneClicked == true)
             {
-                labelMainstory.Text = "HELLO WhiteAss \n\n\n\n\n\n LOADING...";
+                labelMainstory.Text = ".. so you helped the old woman to reach her home and sacrificed minutes of your time. \r\n\r\n\"Thank you for helping me. Please take this bag of fruits as this is the only way I can give my thanks.\" \r\n\r\n\"Ohh.. I appreciate it grandma but I'm fine, thank you!\"\r\n\r\nYou said goodbye to the old woman and still had 5 minutes left, so you did your best to run as fast as you could.\r\n \n\nClick Continue...";
+                btnChoiceOneClicked = false;
+                choice++;
+            }
+            else if (btnChoiceTwoClicked == true) //CHOICE B
+            {
+                labelMainstory.Text = "HELLO Maitim \n\n\n\n\n\nLOADING...";
+                btnChoiceTwoClicked = false;
+                choice++;
+            }
+            else if (btnChoiceThreeClicked == true) //CHOICE C
+            {
+                labelMainstory.Text = "you pretended that you didn't see the old woman and slowly you felt guilty on not helping her. although it saved your reputation in the company for being punctual. (- 1 sanity) \n\nClick Continue...";
+                btnChoiceThreeClicked = false;
+                choice++;
+            }
+        }
+
+        // main story 2
+        // need iconnect sanity b and c
+
+        public void mainStory_2()
+        {
+            labelMainstory.Text = "At work, many of the staffs welcomed you and gave you gifts. You introduced yourself to all of them and after that the manager came in and said,\r\n\r\n\"What's with the commotion? What are you doing in this time? I am expecting that all of you are now working in the kitchen, right?\"\r\n\r\nall of the workers started to go back to their respective stations while the manager head towards you that made you nervous. \r\n\r\n\"So, you're the reason why they are all not in their respective stations, hmm?\r\n\r\nyou nodded. \r\n\r\n\"Don't be nervous kid, I was just acting according to my role. Anyways, welcome to Joblibee. I hope you have a brave heart as your tasks are not for the weak. You may now go to your station.\"\r\n\r\nAfter few minutes, the restaurant is now finally accepting customers. You're co-worker asked you to help him serve the food in table 15, but you also overheard that a family is in need of something. \r\n\r\na. prioritise helping your co-worker\r\nb. help the other customer first, then help your co-worker\r\nc. ask another waiter to help the other table\r\n";
+
+            if (btnChoiceOneClicked == true)
+            {
+                labelMainstory.Text = "the customer got mad as no other waiters are paying attention to their requests. And decided to leave the restaurant. \n\nClick Continue...";
+                btnChoiceOneClicked = false;
+                choice++;
+            }
+            else if (btnChoiceTwoClicked == true) //CHOICE B
+            {
+                labelMainstory.Text = "but the customers in table 15 are asking why is their order incomplete, blaming your co-worker. Your co-worker got angry and decided not to talk to you for the rest of the day. \n(-1 Sanity) \n\nClick Continue...";
+                btnChoiceTwoClicked = false;
+                choice++;
+            }
+            else if (btnChoiceThreeClicked == true) //CHOICE C
+            {
+                labelMainstory.Text = "The manager came out and asked the other staff about the situation. However, the problem was already solved and the customers are now satisfied.\r\n\r\n\"This is the first time that I saw customers this irritated.\" you said, heart pounding, scared that the scenario almost kick you out of your job. \r\n \n(+1 Sanity) \n\nClick Continue...";
+                btnChoiceThreeClicked = false;
+                choice++;
+            }
+        }
+
+        // main story 3
+        // letter b intelligence based di pa nailalagay
+
+        public void mainStory_3()
+        {
+            labelMainstory.Text = "\"Don't worry, we're used to it. It's not our fault that all of us are busy. They can come to the cashier to ask for utensils if there are no waiters available.\" said your co-worker.\r\n\r\n\"oh well.. I'll get used to it someday\"\r\n\r\nIt's already closing hours. You packed up your things, getting ready to go home. You are excited to come home that you're already imagining yourself laying down in your soft bed and watching an interesting movie. \r\n\r\n\"Hey, wanna hang out with us tonight?\" your daydreaming ends as one of your co-workers asked you to go out with them. \r\n\r\na. say yes and go with them\r\nb. lie and told them that your parents are looking for you\r\nc. ignore your co-worker and proceed to go home.\r\n";
+
+            if (btnChoiceOneClicked == true)
+            {
+                labelMainstory.Text = "since this is your first day, you said yes to your co-worker without knowing the location first. all your co-workers are ready to go. It’s near your workplace, so all of you decided to walk instead of riding a tricycle.\r\n\r\nYou are slowly approaching the place they told you and to your surprise, it’s a bar and not a restaurant. \r\n\r\n“You drink, right?” your co-worker asked.\r\n\r\n“To be honest, I don’t drink..” you said, leaving them in disbelief.\r\n“Now’s the time you drink! This is a tradition every time we have a new co-worker” they said.\r\n\r\nYou can’t do anything but to agree since you don’t want to disappoint them.\r\n\r\nThey made you drink the beer, and after many hours of bonding and drinking, it’s time to go home.\r\n \n\nClick Continue...";
                 btnChoiceOneClicked = false;
                 choice++;
             }
@@ -146,22 +224,44 @@ namespace Text_Based_Game_System
             }
             else if (btnChoiceThreeClicked == true) //CHOICE C
             {
-                labelMainstory.Text = "HELLO Snig \n\n\n\n\n\n LOADING...";
+                labelMainstory.Text = "Nervously ignore your co-worker and proceed to go home.\n\nClick Continue...";
                 btnChoiceThreeClicked = false;
                 choice++;
             }
         }
 
-        public void fillerStory_1() //Filler 1
+        // filler 2
+        // stats affected letter a, c
+
+        public void fillerStory_2()
         {
-            labelMainstory.Text = "ARE YOU GAY? \n Choices: \n A. Yes \n B. No \n C. yesn't";
+            labelMainstory.Text = "It’s raining and good thing, you brought an umbrella. While walking, you saw a lady who was struggling in holding a lot of things and didn't have an umbrella. \r\n\r\na. help her\r\nb. ignore her\r\nc. laugh at her\r\n";
 
             if (btnChoiceOneClicked == true)
             {
-                labelMainstory.Text = "gayass faggot";
+                labelMainstory.Text = "you helped the lady and it granted you something and it vanished. (+1 Sanity, -1 health) \n\nClick Continue";
                 btnChoiceOneClicked = false;
                 choice++;
             }
+            else if (btnChoiceTwoClicked == true) //CHOICE B
+            {
+                labelMainstory.Text = "you were dizzy and feeling nauseous, so you ignored the lady. \n\nClick Continue";
+                btnChoiceTwoClicked = false;
+                choice++;
+            }
+            else if (btnChoiceThreeClicked == true) //CHOICE C
+            {
+                labelMainstory.Text = "you were too drunk and laughed at the lady who was soaking wet in the rain. she stared into your soul and felt like she said something you can’t hear. (-1 Sanity, -1 Health) \n\nClick Continue...";
+                btnChoiceThreeClicked = false;
+                choice++;
+            }
+        }
+
+        // main story 4
+
+        public void mainStory_4()
+        {
+            labelMainstory.Text = "You came home tired and longing for your bed. \r\n\r\n“This is so tiring, I almost forgot I need to have time for myself. Maybe I should just do it tomorrow. I really need to take a rest.”\r\n\r\nTo be continued…\r\n";
         }
 
         public void gameStart()
@@ -177,52 +277,33 @@ namespace Text_Based_Game_System
                     break;
 
                 case 3:
-                    labelMainstory.Text = "FILLER (2)";
+                    mainStory_2();
                     break;
 
                 case 4:
-                    labelMainstory.Text = "MAIN GAME STORY (2)";
+                    mainStory_3();
                     break;
 
                 case 5:
-                    labelMainstory.Text = "FILLER (3)";
+                    fillerStory_2();
                     break;
 
                 case 6:
-                    labelMainstory.Text = "FILLER (4)";
+                    mainStory_4();
                     break;
 
-                case 7:
-                    labelMainstory.Text = "MAIN GAME STORY (3)";
-                    break;
+
             }
 
         }
 
-        private void label7_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void AttackBTN_Click(object sender, EventArgs e)
-        {
-            
-        }
-
+        // test button
         private void button1_Click(object sender, EventArgs e)
         {
             gameStart();
         }
 
-        private void automaticRefresh(object sender, EventArgs e)
-        {
-            gameStart();
-        }
 
     }
 }
